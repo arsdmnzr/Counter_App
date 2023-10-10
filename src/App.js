f509a3179e70aca193e5ca5c14494ed32d993f2a
arsdmnzr <you@example.com>
2023-10-11 01:03:25 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [count, setCount] = useState(0);
+  function add() {
+    setCount(count + 1);
+  }
+  function minus() {
+    if (count > 0) setCount(count - 1);
+  }
+  return (<div className='container'>
+    <div className='card'>
+      <h1 className="hi">
+        Counter App
+        <br />
+        {count}
+      </h1>
+      <button onClick={add}>+</button>
+      <button onClick={minus}>-</button>
+    </div></div>
   );
 }
-
 export default App;
